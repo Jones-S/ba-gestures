2890fdb4c51f371bbc9d34d5c5defb362f4914c4
Jones <jonas.scheiwiller@zhdk.ch>
2016-04-06 11:46:31 +0200

    [Refactor] Use the prototype property to define all functions within an object

--- leap-gestures/js/LeapApp.controller.js
+++ leap-gestures/js/LeapApp.controller.js
@@ -2,16 +2,18 @@ LEAPAPP.Controller = function() {
     this.function_name = "controller";
 };
 
-LEAPAPP.Controller.prototype.init = function() {
-    console.log("controller initialized");
 
-    // populate variables
-    this.currentSeg = "seg0";
-};
+LEAPAPP.Controller.prototype = {
+    init: function() {
+        console.log("controller initialized");
+        // populate variables
+        this.currentSeg = "seg0";
+    },
 
-LEAPAPP.Controller.prototype.startTracking = function() {
-    console.log("tracking started");
-    console.log("this: " + this);
-    console.log(JSON.stringify(this,null, 4));
+    startTracking: function() {
+        console.log("tracking started");
+        console.log("this: " + this);
+        console.log(JSON.stringify(this,null, 4));
 
+    }
 };
