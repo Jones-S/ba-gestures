457b69c57861025a3254f7c85050b80ffbd34557
Jones <jonas.scheiwiller@zhdk.ch>
2016-04-11 15:19:26 +0200

    [Update] Add shiftr «class» in my project

---
+++ leap-gestures/js/LeapApp.modules.shiftr.js
@@ -0,0 +1,15 @@
+(function() {
+
+    LEAPAPP.Shiftr = function() {
+        // constructor
+        this.name = "Shiftr";
+
+    };
+
+    LEAPAPP.Shiftr.prototype.init = function() {
+
+    };
+
+
+
+}());
