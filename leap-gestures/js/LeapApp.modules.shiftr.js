a379e4aae1d42c2e4fddca7067ddc8460d01d125
Jones <jonas.scheiwiller@zhdk.ch>
2016-04-12 17:08:32 +0200

    [Update] Send a lamp message on click

--- leap-gestures/js/LeapApp.modules.shiftr.js
+++ leap-gestures/js/LeapApp.modules.shiftr.js
@@ -5,6 +5,7 @@
         this.name = "Shiftr";
         this.mqtt = data.mqtt_uri;
         this.client_id = data.client_id;
+        this.lamp_on = false;
         var uber = this;
 
         this.client = mqtt.connect(this.mqtt, {
@@ -19,15 +20,28 @@
 
         });
 
-       this.client.on('message', function(topic, message) {
+        this.client.on('message', function(topic, message) {
             console.log('new message:', topic, message.toString());
         });
+
+        $("body").on( "click", function() {
+            if (!uber.lamp_on) {
+                uber.client.publish('/lamp', 'on');
+                uber.lamp_on = true;
+                console.log("on");
+            } else {
+                uber.client.publish('/lamp', 'off');
+                uber.lamp_on = false;
+                console.log("off");
+            }
+        });
+
 
     };
 
     LEAPAPP.Shiftr.prototype.publish = function(topic, message) {
         var uber = this;
-        uber.client.publish('/lamp', 'on');
+        // uber.client.publish('/lamp', 'on');
 
     };
 
