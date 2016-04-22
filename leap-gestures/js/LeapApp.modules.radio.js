44a456292f96368f0e0a12fa56f228ea4daa8d2a
Jones <jonas.scheiwiller@zhdk.ch>
2016-04-22 11:07:10 +0200

    [WIP] Problems with playing instances and pausing tracks

--- leap-gestures/js/LeapApp.modules.radio.js
+++ leap-gestures/js/LeapApp.modules.radio.js
@@ -33,20 +33,21 @@
         this.files = completeFilePath(this.folder, this.files);
         console.log("this.files: ", this.files);
 
-        this.current_track = 0;
+        this.current_track          = 0;
+        this.current_playback_id    = undefined;
         // playlistUrls = [
         //         "./audio/cmn-ni3.mp3",
         //         "./audio/cmn-hao3.mp3",
         //         "./audio/cmn-lao3.mp3",
         //         "./audio/cmn-mao3.mp3"
         //     ], // audio list
-        this.howlerBank = [];
+        this.howler_bank = [];
         this.loop = true;
         var uber = this;
 
-        // build up howlerBank:
+        // build up howler_bank:
         this.files.forEach(function(current, i) {
-            uber.howlerBank.push(new Howl({
+            uber.howler_bank.push(new Howl({
                 urls: [uber.files[i]],
                 // execute onEnd when finished and bind this(=uber) context
                 // otherwise the this in the function onEnd will refer to the Howl context
@@ -56,7 +57,7 @@
         });
 
         // initiate the whole :
-        this.howlerBank[0].play();
+        // this.howler_bank[0].play();
 
 
 
@@ -65,22 +66,38 @@
 
     LEAPAPP.Radio.prototype.play = function() {
         var uber = this;
+        console.log("uber.howler_bank[uber.current_track]: ", uber.howler_bank[uber.current_track]);
+        uber.howler_bank[uber.current_track].play( function(sound_id){
+            uber.current_playback_id = sound_id;
+        });
+
     };
 
     LEAPAPP.Radio.prototype.pause = function() {
         var uber = this;
+        // stop all tracks except the current one
+        console.log("uber.howler_bank: ", uber.howler_bank);
+        var all_other_tracks = _.without(uber.howler_bank, uber.howler_bank[uber.current_track]);
+        console.log("all_other_tracks: ", all_other_tracks);
+        uber.howler_bank.forEach(function (current, index) {
+            // body...
+        });
+        uber.howler_bank[uber.current_track].pause(uber.current_playback_id);
+
     };
 
     // playing i+1 audio (= chaining audio files)
     LEAPAPP.Radio.prototype.onEnd = function() {
+        console.log("onend called");
         var uber = this;
         if (uber.loop === true) {
             // check if current track is more than number of all tracks, otherwise reset to 0
-            uber.current_track = (uber.current_track + 1 !== uber.howlerBank.length) ? uber.current_track + 1 : 0;
+            uber.current_track = (uber.current_track + 1 !== uber.howler_bank.length) ? uber.current_track + 1 : 0;
         } else {
             uber.current_track = uber.current_track + 1;
         }
-        uber.howlerBank[uber.current_track].play();
+        uber.howler_bank[uber.current_track].play();
+        console.log("%c ffomr autoplay]", "background: #0D0B07; color: #FAFBFF", uber.howler_bank[uber.current_track]);
     };
 
 }());
