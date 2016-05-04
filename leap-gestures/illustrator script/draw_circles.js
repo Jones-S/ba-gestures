2bc8ee815c61f8f4bbdcde9d0b20fe13a3f8034a
Jones <jonas.scheiwiller@zhdk.ch>
2016-05-04 14:48:08 +0200

    [Update] Illustrator: Draw 6 circles around the first.

--- leap-gestures/illustrator script/draw_circles.js
+++ leap-gestures/illustrator script/draw_circles.js
@@ -5,10 +5,10 @@ function main() {
     var docWidth = 700;
     var docHeight = 700;
     var amountOfArtboards = 1;
-    var circleRadius = 5;
-    var numberOfRings = 3;
-    var horizontalDistance = 13;
-    var amtOfCircInFirstRing = 6; // the amount of the circles in the first ring
+    var circleRadius = 2;
+    var numberOfRings = 1;
+    var horizontalDistance = 19;
+    var amountsOfCircles = 6; // the amount of the circles in the first ring, will increase over time
 
     var centerPoint = {
         x: (docWidth/2) + circleRadius,
@@ -20,6 +20,7 @@ function main() {
         y: centerPoint.y
     };
 
+
     // add a new document
     var doc = app.documents.add(DocumentColorSpace.RGB, docWidth, docHeight, amountOfArtboards);
 
@@ -28,27 +29,46 @@ function main() {
     fillColor.green = 0;
     fillColor.blue  = 0;
 
-    doc.defaultFillColor = fillColor;
     doc.defaultFilled = true;
     doc.defaultStroked = false;
+    doc.defaultFillColor = fillColor;
 
 
     // draw center circle ellipse(['bottom'], [left], [height], [width])
     // bottom is actually top but somehow my ruler is inversed
-    pi = activeDocument.activeLayer.pathItems.ellipse(centerPoint.x, centerPoint.y, circleRadius*2, circleRadius*2);
+    var pi = activeDocument.activeLayer.pathItems.ellipse(centerPoint.y, centerPoint.x, circleRadius*2, circleRadius*2);
 
     // for each ring add circles
     for (var i = numberOfRings - 1; i >= 0; i--) {
-        var distance = 360 / amtOfCircInFirstRing; // with 6 its 30°
+        var angle = 360 / amountsOfCircles; // with 6 its 30°
+        var currentAngle = 0;
+
         // determine first position of circle
-        var firstPoint;
+        var firstPoint = { x: 0, y: 0};
         firstPoint.x = currentPoint.x;
         firstPoint.y = currentPoint.y + horizontalDistance;
+
+        for (var j = amountsOfCircles - 1; j >= 0; j--) {
+            // determine new point
+            var newCenterpoint = calculatePosition(currentAngle, horizontalDistance, centerPoint);
+            // draw new circle
+            var circle = activeDocument.activeLayer.pathItems.ellipse(newCenterpoint.y, newCenterpoint.x, circleRadius*2, circleRadius*2);
+            // and increase angle
+            currentAngle += angle;
+        }
+
     }
 
+}
+
+function calculatePosition(angleInDegrees, radiusOfRing, centerPoint){
 
 
+    // Convert from degrees to radians via multiplication by PI/180
+    var x = (radiusOfRing * Math.cos(angleInDegrees * Math.PI / 180)) + centerPoint.x;
+    var y = (radiusOfRing * Math.sin(angleInDegrees * Math.PI / 180)) + centerPoint.y;
 
+    return {x: x, y: y};
 }
 
 
