dafe24f61d523a9deb03644198e7866836885e92
Jones <jonas.scheiwiller@zhdk.ch>
2016-05-05 16:33:45 +0200

    [WIP] Mutate lamp flow to ventilator flow.

--- leap-gestures/js/LeapApp.flow.ventilator.js
+++ leap-gestures/js/LeapApp.flow.ventilator.js
@@ -1,9 +1,4 @@
-/**
- * Flow defines the interaction Flow in one object. For better code structure the whole flow
- * lies in it's own file.
- */
-
-var LAMPFLOW = {
+var VENTILATORFLOW = {
 
     on_off_count: 0,    // save count and increase if on off
     distinct_count: 0,
@@ -12,7 +7,7 @@ var LAMPFLOW = {
         onEnter: function() {
             this.played_fns.on_enter = true;
             this.flags = {
-                lamp_on:                false,
+                venti_on:                false,
                 first_gesture_saved:    false
             };
             this.brightness = 100;
@@ -26,81 +21,57 @@ var LAMPFLOW = {
             // check if swipe gesture was registered and if the inititial gesture was not explode
             if (this.try(gesture_data, 'swipe') && this.on_off_gesture != 'explode') {
                 if (gesture_data.swipe == 'up') {
-                    if (!this.flags.lamp_on) {
+                    if (!this.flags.venti_on) {
                         // check if gesture option is set and on_off_gesture is not defined yet
                         if (myLeapApp.gesture_option && !this.on_off_gesture) {
                             // save first gesture if gesture option is set
                             this.on_off_gesture = 'swipe';
                             console.log("this.on_off_gesture: ", this.on_off_gesture);
                         }
-                        myLeapApp.shiftr.publish('/lamp', 'on');
+                        myLeapApp.shiftr.publish('/venti', 'on');
                         myLeapApp.sounder.play('on');
                         myLeapApp.flow.on_off_count++;
-                        this.flags.lamp_on = true;
+                        this.flags.venti_on = true;
                     }
                 }
                 else if(gesture_data.swipe == "down") {
-                    if (this.flags.lamp_on) {
+                    if (this.flags.venti_on) {
                         if (myLeapApp.gesture_option && !this.on_off_gesture) {
                             // save first gesture
                             this.on_off_gesture = 'swipe';
                         }
-                        myLeapApp.shiftr.publish('/lamp', 'off');
+                        myLeapApp.shiftr.publish('/venti', 'off');
                         myLeapApp.sounder.play('off');
                         myLeapApp.flow.on_off_count++;
-                        this.flags.lamp_on = false;
+                        this.flags.venti_on = false;
                     }
                 }
             }
             else if(this.try(gesture_data, 'on') && this.on_off_gesture != 'swipe') {
-                if (!this.flags.lamp_on) {
+                if (!this.flags.venti_on) {
                     if (myLeapApp.gesture_option && !this.on_off_gesture) {
                         // save first gesture
                         this.on_off_gesture = 'explode';
                         console.log("this.on_off_gesture: ", this.on_off_gesture);
                     }
-                    myLeapApp.shiftr.publish('/lamp', 'on');
+                    myLeapApp.shiftr.publish('/venti', 'on');
                     myLeapApp.sounder.play('on');
                     myLeapApp.flow.on_off_count++;
-                    this.flags.lamp_on = true;
+                    this.flags.venti_on = true;
                 }
 
             }
             else if(this.try(gesture_data, 'off') && this.on_off_gesture != 'swipe') {
-                if (this.flags.lamp_on) {
+                if (this.flags.venti_on) {
                     if (myLeapApp.gesture_option && !this.on_off_gesture) {
                         // save first gesture
                         this.on_off_gesture = 'explode';
                     }
-                    myLeapApp.shiftr.publish('/lamp', 'off');
+                    myLeapApp.shiftr.publish('/venti', 'off');
                     myLeapApp.sounder.play('off');
                     myLeapApp.flow.on_off_count++;
-                    this.flags.lamp_on = false;
+                    this.flags.venti_on = false;
                 }
-            }
-            // if lamp is on check y axis for dimming
-            if (this.flags.lamp_on) {
-                // set current position of hand to current brightness
-                if (data.hands.length === 1) {
-                    uber.brtn_plsh_count++;
-                    // only publish every x frames
-                    if (uber.brtn_plsh_count % 15 === 0) {
-                        // y-Axis range 120mm – 420mm
-                        // mapping this to brightness
-                        // console.log("data.hands[0].palmPosition[1]: ", data.hands[0].palmPosition[1]);
-                        var y_axis = data.hands[0].palmPosition[1];
-                        this.brightness = y_axis.map(120, 420, 5, 150);
-                        this.brightness = (this.brightness < 5) ? 5 : this.brightness; // 5 is minimum
-                        this.brightness = (this.brightness > 150) ? 150 : this.brightness; // 150 maximum
-                        console.log("this.brightness: ", this.brightness);
-                        myLeapApp.shiftr.publish('/lamp', 'brightness-' + this.brightness);
-                    }
-                    // reset counter every now and then
-                    if (uber.brtn_plsh_count >= 3600) {
-                        uber.brtn_plsh_count = 0;
-                    }
-                }
-
             }
         },
         onLeave: function() {
@@ -257,7 +228,7 @@ var LAMPFLOW = {
             }, 3000);                },
         onGestureCheck: function(gesture_data, data) {
             if (gesture_data.swipe == "up" || gesture_data.swipe == 'down') {
-                // set lamp on in doAlways
+                // set venti on in doAlways
                 myLeapApp.machine.callNextSeg('seg8');
             }
         },
@@ -274,7 +245,7 @@ var LAMPFLOW = {
             }, 3000);                },
         onGestureCheck: function(gesture_data, data) {
             if (gesture_data.swipe == "up" || gesture_data.swipe == 'down') {
-                // set lamp on in doAlways
+                // set venti on in doAlways
                 myLeapApp.machine.callNextSeg('seg8');
             }
         },
