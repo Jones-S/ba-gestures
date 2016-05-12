49792d6eb549aff977532d5e2f2ad54209f524af
Jones <jonas.scheiwiller@zhdk.ch>
2016-05-12 18:59:39 +0200

    [Update] Hide object selection divs again.

--- leap-gestures/js/run.js
+++ leap-gestures/js/run.js
@@ -11,7 +11,7 @@
         var controller_options = {
             debug:              true,
             flow:               flow,
-            start_seg:          'seg0',
+            start_seg:          'seg6',
             mqtt_uri:           'mqtt://e0b7ded5:04f776d89819bfdb@broker.shiftr.io',
             client_id:          'jonas laptop',
             typewriter:         false,
