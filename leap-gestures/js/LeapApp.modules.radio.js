d12162b0046a2a1b364f3aa46217eb36a2b5116e
Jones <jonas.scheiwiller@zhdk.ch>
2016-05-16 12:09:52 +0200

    [Update] Add mp3 version to branch as well.

--- leap-gestures/js/LeapApp.modules.radio.js
+++ leap-gestures/js/LeapApp.modules.radio.js
@@ -31,17 +31,17 @@
             ];
         } else if (myLeapApp.music_folder == 'classics') {
             this.files = [
-                'ec_layla.wav',
+                'ec_layla.mp3',
                 'jh_blues.mp3',
                 'jjc_call_me.mp3',
-                'ad_he_wont.wav',
+                'ad_he_wont.mp3',
                 'nj_goodbye.mp3',
                 'paint_it.mp3',
                 'tribe_award.mp3',
-                'rhcp_cabron.wav',
+                'rhcp_cabron.mp3',
                 'uh_lady.mp3',
                 'racon_steady.mp3',
-                'dire_sultan.wav'
+                'dire_sultan.mp3'
             ];
         }
 
@@ -69,7 +69,7 @@
                 // otherwise the this in the function onEnd will refer to the Howl context
                 onend: uber.onEnd.bind(uber),
                 preload: true,
-                html5: true,
+                // html5: true,
                 volume: uber.current_volume
             }));
         });
