247209884a98f8d14d4b046a226c353eb71b3517
Jones <jonas.scheiwiller@zhdk.ch>
2016-06-05 19:47:08 +0200

    [Refactor] Clean Up Radio flow and rewrite on/off part. Delete swipe on/off.

--- leap-gestures/js/LeapApp.flow.lamp.js
+++ leap-gestures/js/LeapApp.flow.lamp.js
@@ -10,74 +10,27 @@ var LAMPFLOW = {
 
     doAlways: {
         onEnter: function() {
-            this.played_fns.on_enter = true;
             this.flags = {
-                lamp_on:                false,
-                first_gesture_saved:    false
+                lamp_on: false,
             };
             this.brightness = 100;
             this.brightness_publish_count = 0;
-            this.on_off_gesture = false; // container for saving the first on off gesture ['swipe' / 'explode']
 
         },
         onGestureCheck: function(gesture_data, data) {
             var uber = this;
 
-            // check if swipe gesture was registered and if the inititial gesture was not explode
-            if (this.try(gesture_data, 'swipe') && this.on_off_gesture != 'explode') {
-                if (gesture_data.swipe == 'up') {
-                    if (!this.flags.lamp_on) {
-                        // check if gesture option is set and on_off_gesture is not defined yet
-                        if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                            // save first gesture if gesture option is set
-                            this.on_off_gesture = 'swipe';
-                            console.log("this.on_off_gesture: ", this.on_off_gesture);
-                        }
-                        myLeapApp.shiftr.publish('/lamp', 'on');
-                        myLeapApp.sounder.play('on');
-                        myLeapApp.flow.on_off_count++;
-                        this.flags.lamp_on = true;
-                    }
-                }
-                else if(gesture_data.swipe == "down") {
-                    if (this.flags.lamp_on) {
-                        if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                            // save first gesture
-                            this.on_off_gesture = 'swipe';
-                        }
-                        myLeapApp.shiftr.publish('/lamp', 'off');
-                        myLeapApp.sounder.play('off');
-                        myLeapApp.flow.on_off_count++;
-                        this.flags.lamp_on = false;
-                    }
-                }
+            if(this.try(gesture_data, 'on') && uber.flags.lamp_on === false) {
+                myLeapApp.shiftr.publish('/lamp', 'on');
+                myLeapApp.sounder.play('on');
+                this.flags.lamp_on = true;
             }
-            else if(this.try(gesture_data, 'on') && this.on_off_gesture != 'swipe') {
-                if (!this.flags.lamp_on) {
-                    if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                        // save first gesture
-                        this.on_off_gesture = 'explode';
-                        console.log("this.on_off_gesture: ", this.on_off_gesture);
-                    }
-                    myLeapApp.shiftr.publish('/lamp', 'on');
-                    myLeapApp.sounder.play('on');
-                    myLeapApp.flow.on_off_count++;
-                    this.flags.lamp_on = true;
-                }
+            else if(this.try(gesture_data, 'off') && uber.flags.lamp_on === true) {
+                myLeapApp.shiftr.publish('/lamp', 'off');
+                myLeapApp.sounder.play('off');
+                this.flags.lamp_on = false;
+            }
 
-            }
-            else if(this.try(gesture_data, 'off') && this.on_off_gesture != 'swipe') {
-                if (this.flags.lamp_on) {
-                    if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                        // save first gesture
-                        this.on_off_gesture = 'explode';
-                    }
-                    myLeapApp.shiftr.publish('/lamp', 'off');
-                    myLeapApp.sounder.play('off');
-                    myLeapApp.flow.on_off_count++;
-                    this.flags.lamp_on = false;
-                }
-            }
             // if lamp is on check y axis for dimming
             if (this.flags.lamp_on) {
                 // set current position of hand to current brightness
