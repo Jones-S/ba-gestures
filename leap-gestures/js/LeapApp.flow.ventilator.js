d2cd075c31e1e73df96a0cb92d7366e08604ac91
Jones <jonas.scheiwiller@zhdk.ch>
2016-06-05 19:29:29 +0200

    [Update][Refactor] Play start and exit sounds. Only play on/off sounds if its off/on. And delete a bunch of unnecessary stuff.

--- leap-gestures/js/LeapApp.flow.ventilator.js
+++ leap-gestures/js/LeapApp.flow.ventilator.js
@@ -1,78 +1,33 @@
 var VENTILATORFLOW = {
     name: 'ventilator',
-    on_off_count: 0,    // save count and increase if on off
-    distinct_count: 0,
 
     doAlways: {
         onEnter: function() {
             this.played_fns.on_enter =  true;
-            this.flags = {
-                venti_on:               false,
-            };
             this.brightness = 100;
-            this.brtn_plsh_count = 0;
-            this.on_off_gesture = false; // container for saving the first on off gesture ['swipe' / 'explode']
+            this.running = false;
 
         },
         onGestureCheck: function(gesture_data, data) {
             var uber = this;
-
-            // check if swipe gesture was registered and if the inititial gesture was not explode
-            if (this.try(gesture_data, 'swipe') && this.on_off_gesture != 'explode') {
-                if (gesture_data.swipe == 'up') {
-                    if (!this.flags.venti_on) {
-                        // check if gesture option is set and on_off_gesture is not defined yet
-                        if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                            // save first gesture if gesture option is set
-                            this.on_off_gesture = 'swipe';
-                            console.log("this.on_off_gesture: ", this.on_off_gesture);
-                        }
-                        myLeapApp.shiftr.publish('/ventilator', 'on');
-                        myLeapApp.sounder.play('on');
-                        // TODO replace flow.on_off_count with uber.on_off_count > save it into segment
-                        // TODO: on off count only on the gesture, which was saved first
-                        myLeapApp.flow.on_off_count++;
-                        this.flags.venti_on = true;
-                    }
-                }
-                else if(gesture_data.swipe == "down") {
-                    if (this.flags.venti_on) {
-                        if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                            // save first gesture
-                            this.on_off_gesture = 'swipe';
-                        }
-                        myLeapApp.shiftr.publish('/ventilator', 'off');
-                        myLeapApp.sounder.play('off');
-                        myLeapApp.flow.on_off_count++;
-                        this.flags.venti_on = false;
-                    }
-                }
+            if (this.try(gesture_data, 'start')) {
+                myLeapApp.sounder.play('start');
             }
-            else if(this.try(gesture_data, 'on') && this.on_off_gesture != 'swipe') {
-                if (!this.flags.venti_on) {
-                    if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                        // save first gesture
-                        this.on_off_gesture = 'explode';
-                        console.log("this.on_off_gesture: ", this.on_off_gesture);
-                    }
-                    myLeapApp.shiftr.publish('/ventilator', 'on');
-                    myLeapApp.sounder.play('on');
-                    myLeapApp.flow.on_off_count++;
-                    this.flags.venti_on = true;
-                }
 
+            if (this.try(gesture_data, 'exit')) {
+                myLeapApp.sounder.play('exit');
             }
-            else if(this.try(gesture_data, 'off') && this.on_off_gesture != 'swipe') {
-                if (this.flags.venti_on) {
-                    if (myLeapApp.gesture_option && !this.on_off_gesture) {
-                        // save first gesture
-                        this.on_off_gesture = 'explode';
-                    }
-                    myLeapApp.shiftr.publish('/ventilator', 'off');
-                    myLeapApp.sounder.play('off');
-                    myLeapApp.flow.on_off_count++;
-                    this.flags.venti_on = false;
-                }
+
+            if(this.try(gesture_data, 'on') && uber.running == false) {
+                myLeapApp.shiftr.publish('/ventilator', 'on');
+                myLeapApp.sounder.play('on');
+                uber.running = true;
+            }
+            if(this.try(gesture_data, 'off') && uber.running == true) {
+                myLeapApp.shiftr.publish('/ventilator', 'off');
+                myLeapApp.sounder.play('off');
+                uber.running = false;
+
             }
         },
         onLeave: function() {
