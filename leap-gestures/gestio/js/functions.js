6e0220c9e151274e1c42f98c4a980a9f9d8bd6fa
Jones <jonas.scheiwiller@zhdk.ch>
2016-06-09 01:00:36 +0200

    [Update] Write shiftr send code for buttons.

--- leap-gestures/gestio/js/functions.js
+++ leap-gestures/gestio/js/functions.js
@@ -8,5 +8,74 @@
         });
 
 
+
+
+        /*Shiftr Stuff*/
+
+         var client = mqtt.connect('mqtt://e0b7ded5:04f776d89819bfdb@broker.shiftr.io', {
+            clientId: 'Remote Client'
+        });
+
+        client.on('connect', function() {
+            console.log('client has connected!');
+        });
+
+
+
+        /*Radio Buttons*/
+
+        $('#btn--play').on( "click", function() {
+            client.publish('/radio', 'play');
+        });
+
+        $('#btn--pause').on( "click", function() {
+            client.publish('/radio', 'pause');
+        });
+
+        $('#btn--next').on( "click", function() {
+            client.publish('/radio', 'next-track');
+        });
+
+        $('#btn--prev').on( "click", function() {
+            client.publish('/radio', 'prev-track');
+        });
+
+        $('#btn--volup').on( "click", function() {
+            client.publish('/radio', 'vol-up');
+        });
+
+        $('#btn--voldown').on( "click", function() {
+            client.publish('/radio', 'vol-down');
+        });
+
+
+
+
+
+
+
+        // $('').on( "click", function() {
+        //     client.publish('/lamp', 'brighter');
+        //     console.log("lamp led-up");
+        // });
+
+        // $('').on( "click", function() {
+        //     client.publish('/lamp', 'lessbright');
+        //     console.log("lamp led-down");
+        // });
+
+        // $('').on( "click", function() {
+        //     client.publish('/lamp', 'brightness-140');
+        // });
+
+        // $('').on( "click", function() {
+        //     client.publish('/lamp', 'brightness-40');
+        // });
+
+        // $('').on( "click", function() {
+        //     client.publish('/sound', 'on');
+        // });
+
+
     });
 }(jQuery));
